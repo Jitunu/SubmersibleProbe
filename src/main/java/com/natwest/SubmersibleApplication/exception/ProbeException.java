ee92680942829a64e891160255dd5235ef1f00ff
Jitunu Sahoo <jitunusahoo@gmail.com>
2025-02-19 13:25:16 +0000

    Implemented funcationality of ProbeControlService

--- src/main/java/com/natwest/SubmersibleApplication/exception/ProbeException.java
+++ src/main/java/com/natwest/SubmersibleApplication/exception/ProbeException.java
@@ -1,4 +1,7 @@
 package com.natwest.SubmersibleApplication.exception;
 
 public class ProbeException extends RuntimeException{
+    public ProbeException(String s) {
+        super(s);
+    }
 }
--- src/main/java/com/natwest/SubmersibleApplication/service/ProbeControlService.java
+++ src/main/java/com/natwest/SubmersibleApplication/service/ProbeControlService.java
@@ -1,19 +1,24 @@
 package com.natwest.SubmersibleApplication.service;
 
+import com.natwest.SubmersibleApplication.exception.ProbeException;
 import com.natwest.SubmersibleApplication.model.Direction;
 import com.natwest.SubmersibleApplication.model.Position;
-import lombok.Data;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Service
 public class ProbeControlService {
-    private Position position;
+    @Getter
+    private final Position position;
+    @Getter
     private Direction direction;
     private final int gridWidth;
     private final int gridHeight;
 
+    @Getter
     private List<Position> visitedPositions;
 
     public ProbeControlService(int x, int y, Direction direction, int gridWidth, int gridHeight) {
@@ -26,23 +31,65 @@ public class ProbeControlService {
     }
 
     public void moveForward() {
+        int newX = position.getX();
+        int newY = position.getY();
 
+        switch (direction) {
+            case NORTH -> newY++;
+            case EAST -> newX++;
+            case SOUTH -> newY--;
+            case WEST -> newX--;
+        }
+
+        if (isValidPosition(newX, newY)) {
+            position.setX(newX);
+            position.setY(newY);
+            visitedPositions.add(new Position(newX, newY));
+        } else {
+            throw new ProbeException("Probe cannot move outside the grid.");
+        }
     }
 
     public void moveBackward() {
+        int newX = position.getX();
+        int newY = position.getY();
 
+        switch (direction) {
+            case NORTH -> newY--;
+            case EAST -> newX--;
+            case SOUTH -> newY++;
+            case WEST -> newX++;
+        }
+
+        if (isValidPosition(newX, newY)) {
+            position.setX(newX);
+            position.setY(newY);
+            visitedPositions.add(new Position(newX, newY));
+        } else {
+            throw new ProbeException("Probe cannot move outside the grid.");
+        }
     }
 
     public void turnLeft() {
-
+        direction = switch (direction) {
+            case NORTH -> Direction.WEST;
+            case EAST -> Direction.NORTH;
+            case SOUTH -> Direction.EAST;
+            case WEST -> Direction.SOUTH;
+        };
     }
 
     public void turnRight() {
-
+        direction = switch (direction) {
+            case NORTH -> Direction.EAST;
+            case EAST -> Direction.SOUTH;
+            case SOUTH -> Direction.WEST;
+            case WEST -> Direction.NORTH;
+        };
     }
 
     private boolean isValidPosition(int x, int y) {
-        return false;
+        return x >= 0 && x < gridWidth && y >= 0 && y < gridHeight;
     }
 
 }
